2b58205eaab542d4d101a4c89faafad8b280a45b
Waseem Imtiaz <waseemimtiaz10@gmail.com>
2020-02-11 01:05:34 +0500

    initial commit

--- app/src/main/java/com/jsb/project/FloatingButton.java
+++ app/src/main/java/com/jsb/project/FloatingButton.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.RectF;
 import android.os.Build;
 import android.util.AttributeSet;
 import android.view.View;
@@ -59,15 +60,40 @@ public class FloatingButton extends View {
 
 
 
+        int startTop = 0;
+        int startLeft = 0;
+        int endBottom = getHeight() / 2;
+        int endRight = endBottom;// This makes an equal square.
+
+        int centerX = getWidth() / 2;
+        int centerY = getHeight() / 2;
+
+        int upperEdgeX = (int) (centerX + getWidth() / 2 * Math.cos(270 * Math.PI / 180));
+        int upperEdgeY = (int) (centerY + getWidth() / 2 * Math.sin(270 * Math.PI / 180));
+
+        int bottomEdgeX = (int) (centerX + getWidth() / 2 * Math.cos(90 * Math.PI / 180));
+        int bottomEdgeY = (int) (centerY + getWidth() / 2 * Math.sin(90 * Math.PI / 180));
+
+        int leftEdgeX = (int) (centerX + getWidth() / 2 * Math.cos(180 * Math.PI / 180));
+        int leftEdgeY = (int) (centerY + getWidth() / 2 * Math.sin(180 * Math.PI / 180));
+
+        int rightEdgeX = (int) (centerX + getWidth() / 2 * Math.cos(0 * Math.PI / 180));
+        int rightEdgeY = (int) (centerY + getWidth() / 2 * Math.sin(0 * Math.PI / 180));
+
+        RectF rect = new RectF(startTop, startLeft, endRight, endBottom);
+        canvas.drawCircle(centerX, centerY, getWidth() / 2, mPaint);
+
+
 
 //        canvas.drawPaint(paint);
-        mPaint.setStrokeWidth(20);
+        mPaint.setStrokeWidth(15);
         mPaint.setColor(0xffdaedf0);
         mPaint.setAntiAlias(true);
         mPaint.setStyle(Paint.Style.STROKE);
         paint.setColor(0xffdaedf0);
-
-        canvas.drawCircle(150f,150f, 80, mPaint);
+        mPaint.setColor(0xffb1d0d6);
+        canvas.drawCircle(150f,150f, 55, mPaint);
+        canvas.drawCircle(150f,150f, 90, mPaint);
         canvas.drawCircle(150f,150f, 50, paint);
         paint.setColor(0xff385a7c);
         canvas.drawText("+",125f,165f,paint);
