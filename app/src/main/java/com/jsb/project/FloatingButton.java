a3cc2478be7c869662b69445e1ca55969591827f
Waseem Imtiaz <waseemimtiaz10@gmail.com>
2020-02-11 21:11:30 +0500

    layout commit

--- app/src/main/java/com/jsb/project/FloatingButton.java
+++ app/src/main/java/com/jsb/project/FloatingButton.java
@@ -4,7 +4,9 @@ package com.jsb.project;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.CornerPathEffect;
 import android.graphics.Paint;
+import android.graphics.RectF;
 import android.os.Build;
 import android.util.AttributeSet;
 import android.view.View;
@@ -16,6 +18,16 @@ public class FloatingButton extends View {
 
     Paint paint;
     Paint mPaint;
+    Paint m2paint;
+    RectF oval;
+    int progress = 0;
+    int baseRadius = 50;
+    int strokeWidth = 15;
+    int state =3;
+    public void setProgress(int progress) {
+        this.progress = progress;
+        invalidate();
+    }
 
     public FloatingButton(Context context) {
         super(context);
@@ -42,43 +54,96 @@ public class FloatingButton extends View {
   public void iniatializer(){
       paint = new Paint();
       mPaint = new Paint();
+      m2paint=new Paint();
+      oval = new RectF(150f - (baseRadius+20), 150f - (baseRadius+20), 150f + (baseRadius+20), 150f + (baseRadius+20));
+
   }
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.drawColor(0xffffffff);
-        float x = getX();
-        float y = getY();
-        paint.setStyle(Paint.Style.FILL);
-        paint.setColor(Color.BLACK);
-        paint.setAntiAlias(true);
-        paint.setTextSize(55);
-        paint.setFakeBoldText(true);
 
 
+        if (state == 1) {
+            paint.setStyle(Paint.Style.FILL);
+            paint.setAntiAlias(true);
+            paint.setTextSize(baseRadius + 5);
+            paint.setFakeBoldText(true);
+            mPaint.setStrokeWidth(strokeWidth);
+            mPaint.setColor(0xffdaedf0);
+            mPaint.setAntiAlias(true);
+            mPaint.setStyle(Paint.Style.STROKE);
+            paint.setColor(0xffdaedf0);
+            mPaint.setColor(0xffb1d0d6);
+
+            paint.setShadowLayer(baseRadius - 41, 0, 0, 0xff000000);
 
 
+            mPaint.setColor(0xffedf2ee);
+            mPaint.setStyle(Paint.Style.FILL);
+            canvas.drawCircle(150f, 150f, baseRadius + 40, mPaint);
+            canvas.drawCircle(150f, 150f, baseRadius, paint);
+            paint.setColor(0xff385a7c);
+            paint.setShadowLayer(0, 0, 0, 0x00000000);
+
+            canvas.drawText("+", 125f, 170f, paint);
+            paint.setColor(0xffff0000);
+            canvas.drawText("1", 150f, 170f, paint);
+            mPaint.setColor(0xffffffff);
+            mPaint.setStyle(Paint.Style.STROKE);
+            mPaint.setStrokeWidth(strokeWidth - 5);
+            canvas.drawCircle(150f, 150f, baseRadius - 10, mPaint);
+
+            mPaint.setStyle(Paint.Style.STROKE);
+            mPaint.setStrokeWidth(strokeWidth - 2);
+            mPaint.setColor(0xffffffff);
+
+            canvas.drawCircle(150f, 150f, baseRadius + 20, mPaint);
+
+            mPaint.setDither(true);
+            mPaint.setStyle(Paint.Style.STROKE);
+            mPaint.setStrokeJoin(Paint.Join.ROUND);
+            mPaint.setStrokeCap(Paint.Cap.ROUND);
+            mPaint.setPathEffect(new CornerPathEffect(baseRadius));
+            mPaint.setAntiAlias(true);
+            mPaint.setColor(Color.RED);
+            canvas.drawArc(oval, -90, -1f + (3.6f * progress), false, mPaint);
 
 
-//        canvas.drawPaint(paint);
-        mPaint.setStrokeWidth(15);
-        mPaint.setColor(0xffdaedf0);
-        mPaint.setAntiAlias(true);
-        mPaint.setStyle(Paint.Style.STROKE);
-        paint.setColor(0xffdaedf0);
-        mPaint.setColor(0xffb1d0d6);
-        canvas.drawCircle(150f,150f, 55, mPaint);
-        canvas.drawCircle(150f,150f, 90, mPaint);
-        canvas.drawCircle(150f,150f, 50, paint);
-        paint.setColor(0xff385a7c);
-        canvas.drawText("+",125f,165f,paint);
-        paint.setColor(0xffff0000);
-        canvas.drawText("1",150f,165f,paint);
+        }
+        else if(state==2){
+            paint.setAntiAlias(true);
+            mPaint.setAntiAlias(true);
+            m2paint.setTextSize(baseRadius-10);
+            m2paint.setFakeBoldText(false);
+            mPaint.setColor(0xffedf2ee);
+            mPaint.setStyle(Paint.Style.FILL);
+            canvas.drawCircle(150f, 150f, baseRadius, mPaint);
+            paint.setColor(0xffdbd5d7);
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setStrokeWidth(strokeWidth-3);
+            canvas.drawCircle(150f, 150f, baseRadius-10, paint);
+            m2paint.setColor(0xff385a7c);
+            canvas.drawText("+",130f,162f,m2paint);
+            m2paint.setColor(0xffff0000);
+            canvas.drawText("1", 150f, 162f, m2paint);
 
-
-
-
-
-
+        }
+        else if(state==3){
+            paint.setAntiAlias(true);
+            mPaint.setAntiAlias(true);
+            m2paint.setTextSize(baseRadius-10);
+            m2paint.setFakeBoldText(true);
+            mPaint.setColor(0xff002266);
+            mPaint.setStyle(Paint.Style.FILL);
+            canvas.drawCircle(150f, 150f, baseRadius, mPaint);
+            paint.setColor(0xff666666);
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setStrokeWidth(strokeWidth-3);
+            canvas.drawCircle(150f, 150f, baseRadius-10, paint);
+            m2paint.setColor(0xffffffff);
+            canvas.drawText("+",130f,165f,m2paint);
+            m2paint.setColor(0xffff0026);
+            canvas.drawText("1", 150f, 162f, m2paint);
+        }
     }
 }
--- app/src/main/java/com/jsb/project/TestActivity.java
+++ app/src/main/java/com/jsb/project/TestActivity.java
@@ -4,7 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.BounceInterpolator;
+import android.widget.Toast;
 
+import com.thekhaeng.pushdownanim.PushDownAnim;
+
+import static com.thekhaeng.pushdownanim.PushDownAnim.MODE_SCALE;
+import static com.thekhaeng.pushdownanim.PushDownAnim.MODE_STATIC_DP;
 
 
 public class TestActivity extends AppCompatActivity {
@@ -14,6 +26,52 @@ public class TestActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
         ConstraintLayout root = findViewById(R.id.root);
+        final FloatingButton fb = findViewById(R.id.floatingBtn);
+
+        final Animation animation= AnimationUtils.loadAnimation(this,R.anim.alpha);
+        final Animation animation1= AnimationUtils.loadAnimation(this,R.anim.alpha);
+        animation.setInterpolator(new BounceInterpolator());
+
+        fb.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                v.startAnimation(animation);
+
+                final int[] finalProgress = new int[1];
+                finalProgress[0] = 0;
+                final Handler handler = new Handler(Looper.getMainLooper());
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        fb.setProgress(finalProgress[0]++);
+                        if(fb.progress < 100) {
+                            handler.postDelayed(this, 50);
+                        }
+                    }
+                }, 50);
+
+                return false;
+            }
+        });
+
+
+
+
+        fb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                     v.startAnimation(animation1);
+
+
+            }
+        });
+
+
+
+
+
+
     }
 }
 
